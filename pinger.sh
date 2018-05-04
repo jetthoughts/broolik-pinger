a05d744271a4ce38995d10ec3678b99b7eeb0770
Michael Nikitochkin <miry.sof@gmail.com>
2018-05-04 08:46:30 +0200

    Increased interval.

--- pinger.sh
+++ pinger.sh
@@ -1,6 +1,6 @@
 set -x
 
 while [ true ]; do
-  sleep 120
+  sleep 600
   curl -s -H 'Content-Type: application/json' -H 'Accept: application/json'  -d '{"link": {"url": "http://google.com", "device": "android"}}' https://www.broolik.tk/api/v1/links
 done
