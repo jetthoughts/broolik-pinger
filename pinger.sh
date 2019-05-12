5b5910e7dc99a9bc11915f6f296dc9617c74c96b
Michael Nikitochkin <michael@jetthoughts.com>
2019-05-12 16:46:25 +0200

    Update request

--- pinger.sh
+++ pinger.sh
@@ -2,5 +2,5 @@ set -x
 
 while [ true ]; do
   sleep 600
-  curl -s -H 'Content-Type: application/json' -H 'Accept: application/json'  -d '{"link": {"url": "http://google.com", "device": "android"}}' https://www.broolik.tk/api/v1/links
+  curl -s -H 'Content-Type: application/json' -H 'Accept: application/json'  -d '{"url": "http://google.com", "device": "android"}' https://broolik.tk/api/v1/links
 done
